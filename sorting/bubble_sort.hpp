01ea2f49ba53acc9e75e089ec005357a42162977
Simon Racz <simonracz@gmail.com>
2014-02-11 01:10:00 +0100

    Added simple bubble sort.

--- sorting/bubble_sort.hpp
+++ sorting/bubble_sort.hpp
@@ -9,7 +9,13 @@ namespace athene {
 template <typename T>
 void bubble_sort(std::vector<T>& vec)
 {
-	
+	for (int j=vec.size()-1; j>0; --j) {
+		for (int i=0; i<j; ++i) {
+			if (vec[i]>vec[i+1]) {
+				std::swap(vec[i], vec[i+1]);
+			}			
+		}
+	}	
 }
 
 } //namespace
