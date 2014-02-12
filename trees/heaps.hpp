fa0c625c47e13bb5447cfc5e8d15c92aa9acff14
Simon Racz <simonracz@gmail.com>
2014-02-12 05:32:53 +0100

    Added make_bin_heap template + tests.

---
+++ trees/heaps.hpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <vector>
+
+namespace athene {
+
+template<typename T>
+void max_heapify(std::vector<T>&,int);
+	
+template<typename T>
+void make_bin_heap(std::vector<T>& vec)
+{
+	for (int i = (vec.size()/2 - 1) ; i>=0; --i) {
+		max_heapify(vec, i);
+	}
+}
+
+template<typename T>
+void max_heapify(std::vector<T>& vec, int index)
+{
+	if ((2*index + 1) > (vec.size() - 1)) {
+		return;
+	}
+		
+	if ((2*index + 2) > (vec.size() - 1)) {
+		if (vec[2*index + 1] > vec[index]) {
+			std::swap(vec[2*index + 1], vec[index]);
+		}
+		return;
+	}
+	
+	int largest = index;
+	
+	if (vec[2*index + 1] > vec[largest]) largest = 2*index + 1;
+	if (vec[2*index + 2] > vec[largest]) largest = 2*index + 2;
+
+	if (largest!=index) {
+		std::swap(vec[index], vec[largest]);
+		max_heapify(vec, largest);
+	}
+	
+	return;
+}
+
+}
---
+++ trees/test/heaps_test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <chrono>
+#include <iterator>
+#include "heaps.hpp"
+
+TEST(HeapsTest, random100)
+{
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::mt19937 m(seed);
+	std::uniform_real_distribution<double> dist(0.0,1.0);
+	auto dice = std::bind(dist, m);
+	
+	std::vector<double> vec;
+	vec.reserve(100);
+	std::generate_n(back_inserter(vec), 100, [&dice] {return dice();});
+
+	athene::make_bin_heap(vec);
+	EXPECT_TRUE(std::is_heap(vec.begin(), vec.end()));
+	ASSERT_TRUE(vec.size() == 100);
+	
+	std::shuffle(vec.begin(), vec.end(), m);
+	athene::make_bin_heap(vec);
+	EXPECT_TRUE(std::is_heap(vec.begin(), vec.end()));
+}
