2629ced48e1d0a73bab378bce761208e9125696f
Simon Racz <simonracz@gmail.com>
2014-05-23 17:57:04 +0200

    Abandoned BigInt class.

---
+++ math/big_int.hpp
@@ -0,0 +1,36 @@
+#ifndef big_int_hpp
+#define big_int_hpp
+
+/*
+ 
+#include <vector>
+
+namespace athene {
+
+class BigInt
+{
+public:
+	explicit BigInt(long long value = 0);
+	BigInt(char* digits, size_t size);
+	
+	void clear();
+	size_t num_of_digits();
+	
+	
+	
+	BigInt operator+(const BigInt& theOther);
+	
+	
+	
+	
+	
+	
+};
+
+std::ostream& operator<< (std::ostream& os, const BigInt& bigInt);
+
+}
+
+*/
+ 
+#endif
